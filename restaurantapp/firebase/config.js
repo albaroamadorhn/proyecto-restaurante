f1cc2ab8cd466d10090c894dfe7df6764df4cb22
Albaro <alvaro>
2022-01-08 14:42:01 -0600

    first commit

---
+++ restaurantapp/firebase/config.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0m32HNmDOPT-H_XEE2YICHb-7wn2eVRI",
+    authDomain: "restaurante-12297.firebaseapp.com",
+    projectId: "restaurante-12297",
+    storageBucket: "restaurante-12297.appspot.com",
+    messagingSenderId: "203493292865",
+    appId: "1:203493292865:web:a06198169d5375fc93ffc0",
+    measurementId: "G-YG988D8KBX"
+  };
+
+
+ 
+
+
+
+export default firebaseConfig;
